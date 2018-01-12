80c54d80cf5203241bd32c4375b4c9ab97351910
Parker Catalano <catalano.work@gmail.com>
2018-01-12 20:49:45 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -41,6 +41,14 @@ class Student
       DB[:conn].execute(sql, self.name, self.grade)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
     end
+  end
+
+  def update 
+    sql = <<-SQL
+      UPDATE students SET name = ?, grade = ? WHERE id = ?
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
 
 
 end
